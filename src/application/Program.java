09501d1e099832ae8e42b67d9c6349f520b958d7
daniel-liberto <daniel.liberto@hotmail.com>
2023-11-02 11:35:17 -0300

    Project created

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package src.application;
+
+import java.util.Locale;
+
+public class Program {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+
+    System.out.println("First commit");
+  }
+}
