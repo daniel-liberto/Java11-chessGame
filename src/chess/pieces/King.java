92eadc99c37fdc72fee0db6b54361148f95f07fe
daniel-liberto <daniel.liberto@hotmail.com>
2023-11-04 14:17:02 -0300

    king and rook pieces are showing on the board

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,2 +1,16 @@
-package src.chess.pieces;public class King {
+package src.chess.pieces;
+
+import src.boardgame.Board;
+import src.chess.ChessPiece;
+import src.chess.Color;
+
+public class King extends ChessPiece {
+  public King(Board board, Color color){
+    super(board, color);
+  }
+
+  @Override
+  public String toString(){
+    return "K";
+  }
 }
