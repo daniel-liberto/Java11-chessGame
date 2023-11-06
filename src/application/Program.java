7591675cb8d22d7824dfc0827475b2b48d1c957b
daniel-liberto <daniel.liberto@hotmail.com>
2023-11-06 15:47:46 -0300

    pieces can now move but are in initial stage

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,14 +3,27 @@ package src.application;
 import src.boardgame.Board;
 import src.boardgame.Position;
 import src.chess.ChessMatch;
+import src.chess.ChessPiece;
+import src.chess.ChessPosition;
 
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Program {
   public static void main(String[] args) {
     Locale.setDefault(Locale.US);
 
+    Scanner sc = new Scanner(System.in);
     ChessMatch chessMatch = new ChessMatch();
-    UI.printBoard(chessMatch.getPieces());
+
+    while(true){
+      UI.printBoard(chessMatch.getPieces());
+      System.out.print("\nSource: ");
+      ChessPosition source = UI.readChessPosition(sc);
+      System.out.print("\nTarget: ");
+      ChessPosition target = UI.readChessPosition(sc);
+
+      ChessPiece capturedPiece  = chessMatch.performChessMove(source, target);
+    }
   }
 }
