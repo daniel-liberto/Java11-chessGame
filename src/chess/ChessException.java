cda75b1cde60496ff2331d1b7e658a87838b6c98
daniel-liberto <daniel.liberto@hotmail.com>
2023-11-06 14:32:33 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,8 @@
+package src.chess;
+
+public class ChessException extends RuntimeException {
+  private static final long serialUID = 1L;
+  public ChessException(String msg){
+    super(msg);
+  }
+}
